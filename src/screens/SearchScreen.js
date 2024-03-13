53cdfa0a82aec183cc2ad43537aa061b0d89b47b
Mohammed Usama <usamabhakrani@gmail.com>
2024-03-13 12:09:18 +0500

    SearchScreen Nav

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+const SearchScreen = () => {
+  return (
+    <View style={styles.search}>
+      <Text>SearchScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  search: {},
+});
+
+export default SearchScreen;
